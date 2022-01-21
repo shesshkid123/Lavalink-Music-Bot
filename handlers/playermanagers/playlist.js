7ee5af17618e482d0439caa4a197b1b116bf2849
hpriyam8 <88769893+hpriyam8@users.noreply.github.com>
2022-01-21 13:06:54 +0530

    Update playlist.js

--- handlers/playermanagers/playlist.js
+++ handlers/playermanagers/playlist.js
@@ -104,12 +104,3 @@ async function playlist(client, message, args, type) {
 }
 
 module.exports = playlist;
-/**
- * @INFO
- * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
- * @INFO
- * Work for Milrato Development | https://milrato.eu
- * @INFO
- * Please mention Him / Milrato Development, when using this Code!
- * @INFO
- */
