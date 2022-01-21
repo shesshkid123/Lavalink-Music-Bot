7951e8ac764482945899b064801bce3a6c5201e0
hpriyam8 <88769893+hpriyam8@users.noreply.github.com>
2022-01-21 13:01:59 +0530

    Update prefix.js

--- commands/⚙️ Settings/prefix.js
+++ commands/⚙️ Settings/prefix.js
@@ -17,20 +17,20 @@ module.exports = {
     if (!args[0])
       return message.channel.send(new MessageEmbed()
         .setColor(es.wrongcolor).setFooter(es.footertext, es.footericon)
-        .setTitle(`<a:Deny:863000078690811905> Please provide a new prefix!`)
+        .setTitle(`:x: Please provide a new prefix!`)
         .setDescription(`Current prefix: \`${prefix}\``)
       );
     //if there are multiple arguments
     if (args[1])
       return message.channel.send(new MessageEmbed()
         .setColor(es.wrongcolor).setFooter(es.footertext, es.footericon)
-        .setTitle(`<a:Deny:863000078690811905> The Prefix Can\'t have two spaces`)
+        .setTitle(`:x: The Prefix Can\'t have two spaces`)
       );
     //if the prefix is too long
     if (args[0].length > 5)
       return message.channel.send(new MessageEmbed()
         .setColor(es.wrongcolor).setFooter(es.footertext, es.footericon)
-        .setTitle(`<a:Deny:863000078690811905> The Prefix Can\'t be Longer Then \`5\``)
+        .setTitle(`:x: The Prefix Can\'t be Longer Then \`5\``)
       );
     //set the new prefix
     client.settings.set(message.guild.id, args[0], `prefix`);
@@ -38,25 +38,16 @@ module.exports = {
     return message.channel.send(new MessageEmbed()
       .setColor(es.color).setThumbnail(es.thumb ? es.footericon : null)
       .setFooter(es.footertext, es.footericon)
-      .setTitle(`<a:yes:863000006609076245> Set New Prefix To **\`${args[0]}\`**`)
+      .setTitle(`:white_check_mark: Set New Prefix To **\`${args[0]}\`**`)
     );
   } catch (e) {
       console.log(String(e.stack).bgRed)
       return message.channel.send(new MessageEmbed()
           .setColor(es.wrongcolor)
 					.setFooter(es.footertext, es.footericon)
-          .setTitle(`<a:Deny:863000078690811905> An error occurred`)
+          .setTitle(`:x: An error occurred`)
           .setDescription(`\`\`\`${String(JSON.stringify(e)).substr(0, 2000)}\`\`\``)
       );
   }
   }
 };
-/**
-  * @INFO
-  * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
-  * @INFO
-  * Work for Milrato Development | https://milrato.eu
-  * @INFO
-  * Please mention Him / Milrato Development, when using this Code!
-  * @INFO
-*/
