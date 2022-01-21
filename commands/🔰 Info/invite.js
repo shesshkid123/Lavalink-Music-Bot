f5a25aabfc199c49ead8284398d3f8e03f98f890
hpriyam8 <88769893+hpriyam8@users.noreply.github.com>
2022-01-21 12:38:30 +0530

    Update invite.js

--- commands/🔰 Info/invite.js
+++ commands/🔰 Info/invite.js
@@ -14,16 +14,16 @@ module.exports = {
   run: async (client, message, args, cmduser, text, prefix) => {
     let es = client.settings.get(message.guild.id, "embed")
     try {
-      let button_support_dc = new MessageButton().setStyle('url').setLabel('Support Server').setURL("https://discord.com/invite/FQGXbypRf8")
+      let button_support_dc = new MessageButton().setStyle('url').setLabel('Support Server').setURL("https://discord.gg/gAGX7mdpkD")
       let button_invite = new MessageButton().setStyle('url').setLabel('Invite this Bot').setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
       //array of all buttons
       const allbuttons = [button_support_dc, button_invite]
       message.channel.send({
         embed: new MessageEmbed()
           .setColor(ee.color)
-          .setTitle("Thanks for inviting Milrato Development")
-          .addField(`Bot Powered by Milrato Development`, `**[Invite Public Bot](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)  •  [Support Server/Get your Own Bot](https://discord.com/invite/FQGXbypRf8)**\n\n[*Invite* **${client.user.username}**](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
-          .setFooter("Milrato Development", "https://media.discordapp.net/attachments/845646789020745738/856873122862727208/standard.gif?width=351&height=45"),
+          .setTitle("Thanks for inviting Habibi Music")
+          .addField(`Bot Powered by HPRIYAM`, `**[Invite Me](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)  •  [Support Server](https://discord.gg/gAGX7mdpkD)**\n\n[*Get your Own Bot* **${client.user.username}**](https://discord.gg/JHJDpW7Vgx)`)
+          .setFooter("Habibi Music"),
         buttons: allbuttons
       });
     } catch (e) {
